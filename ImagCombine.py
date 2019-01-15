48163e092bd651dd065d8f91d4fd8de4ca912665
shiwuzhang <zsw18@mails.tsinghua.edu.cn>
2019-01-15 09:03:23 +0800

    This script is used in LBC data reduction pipline. Its main function is combine the image we already have and remove the fringe in it.

--- ImagCombine.py
+++ ImagCombine.py
@@ -16,13 +16,19 @@ print(fitslistname)
 fitslist=open(fitslistname).readlines()
 
 objmasklist=[]
+backgroundlist=[]
 
 #mask the most bright source to make the fringe image better
 for i in range(len(fitslist)):
       print('sextractor fits '+str(i)+'.....')
-      sex='sex '+fitslist[i][:-1]+' -c bok001.sex -SATUR_LEVEL 60000 -DETECT_MINAREA 5 -DETECT_MAXAREA 5000 -DETECT_THRESH 2.2 -ANALYSIS_THRESH 0.2 -FILTER Y -FILTER_NAME gauss_2.5_5x5.conv -CHECKIMAGE_TYPE -OBJECTS -CHECKIMAGE_NAME '+'obj_'+fitslist[i]
+      sex1='sex '+fitslist[i][:-1]+' -c bok001.sex -SATUR_LEVEL 60000 -DETECT_MINAREA 5 -DETECT_MAXAREA 5000 -DETECT_THRESH 2.2 -ANALYSIS_THRESH 0.2 -FILTER Y -FILTER_NAME gauss_2.5_5x5.conv -CHECKIMAGE_TYPE -OBJECTS -CHECKIMAGE_NAME '+'obj_'+fitslist[i]
       objmasklist.append('obj_'+fitslist[i][:-1])
-      os.system(sex)
+      os.system(sex1)
+      sex2='sex '+fitslist[i][:-1]+' -c bok001.sex -SATUR_LEVEL 60000 -DETECT_MINAREA 5 -DETECT_MAXAREA 5000 -DETECT_THRESH 2.2 -ANALYSIS_THRESH 0.2 -FILTER Y -FILTER_NAME gauss_2.5_5x5.conv -CHECKIMAGE_TYPE BACKGROUND -CHECKIMAGE_NAME '+'bk_'+fitslist[i]
+      backgroundlist.append('bk_'+fitslist[i][:-1])
+      os.system(sex2)
+     
+    
 
 
 
@@ -30,22 +36,29 @@ for i in range(len(fitslist)):
 
 #make fringe image by median,because it adopt dither mode
 outimg=fits.HDUList()
+backimg=fits.HDUList()
 for i in range(1,5):
     print('the '+str(i)+'ext.....')
     ext=[]
+    back=[]
     for fitsi in range(len(objmasklist)):
-        print('the '+str(fitsi)+' fits......')
-        fitsfile=fits.open(objmasklist[fitsi])
-        # header=fitsfile.header
-        ext.append(fitsfile[i].data)
-        fitsfile.close()
+	    print('the '+str(fitsi)+' fits......')
+	    fitsfile=fits.open(objmasklist[fitsi])
+	    backfile=fits.open(backgroundlist[fitsi])
+	    ext.append(fitsfile[i].data)
+	    back.append(backfile[i].data)
+	    backfile.close()
+	    fitsfile.close()
     ext=np.array(ext)
+    back=np.array(back)
     new_ext=np.median(ext,axis=0)
-    new_ext=new_ext/(np.median(new_ext))
+    new_back=np.median(back,axis=0)
     outimg.append(fits.ImageHDU(new_ext))
+    backimg.append(fits.ImageHDU(new_back))
 
 print(len(outimg))
-outimg.writeto('new.fits',overwrite=True)
+backimg.writeto('background.fits',overwrite=True)
+outimg.writeto('fringe.fits',overwrite=True)
 print('done')
 os.system('rm obj*fits')
 
