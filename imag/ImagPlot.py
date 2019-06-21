71d0b089584d4206bee42232b5ee2635d1996767
shiwuzhang <zsw18@mails.tsinghua.edu.cn>
2019-06-21 13:57:44 +0800

    rewrite the twodplot function

--- imag/ImagPlot.py
+++ imag/ImagPlot.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
 from astropy.visualization import ZScaleInterval,LinearStretch,ImageNormalize
-from pylab import pcolor,colorbar
 from mpl_toolkits.mplot3d import Axes3D
 from cube import CubeData
 from . import ImgInterSmo
@@ -30,11 +28,32 @@ def PlotMap(cubedata,emissionline=None):
 
     return map
 
-def Twodplotimg(map,x,y,vmin=None,vmax=None):
-    plt.figure()
-    ax=pcolor(y,x,map.T,cmap=cm.jet,vmax=vmax,vmin=vmin)
-    cbar=colorbar()
-    return ax,cbar
+def Twodplotimg(map,x,y,subrow=1,subclo=1,vmin=None,vmax=None,markpoint=None,xlabel=r'x',ylabel=r'y',cbarlabel='cbar'):
+    fig,AX=plt.subplots(subrow,subclo,num=len(map))
+    fig.text(0.5, 0.04, xlabel, ha='center')
+    fig.text(0.04, 0.5, ylabel, va='center', rotation='vertical')
+    AX=AX.flatten()
+    l_map=0
+    if (subclo is 1) and (subrow is 1):
+        AX=[AX]
+        map=[map]
+    for i in range(len(AX)):
+        if l_map<=len(map)-1:
+            img=AX[i].pcolor(y,x,map[l_map].T,cmap='jet',vmax=vmax,vmin=vmin)
+        else:
+            fig.delaxes(AX[i])
+        l_map+=1
+        if markpoint is not None:
+            AX[i].scatter(0.,0.,marker='*',color='magenta',s=100)
+
+    cax = fig.add_axes([0.92, 0.1, 0.02, 0.8])
+    cbar=fig.colorbar(img,cax)
+    cbar.set_label('$velocity \ km/s)$')
+    fig.tight_layout(rect=[0,0,.92,1])
+    plt.show()
+
+
+    return None
 
 def Mapprepro(twodmap,x,y,internum):
     twodmap = ImgInterSmo.InSmimg(twodmap, internum, [3., 3.])
@@ -45,7 +64,7 @@ def Threedplotimg(twodmap,x,y):
     X, Y = np.meshgrid(x, y)
     fig = plt.figure()
     ax = Axes3D(fig)
-    img=ax.plot_surface(X, Y, twodmap, rstride=1, cstride=1, cmap=cm.jet)
+    img=ax.plot_surface(X, Y, twodmap, rstride=1, cstride=1, cmap='jet')
     return img
 
 def Cutmap(datacube,cutaxis,waverange,xaxis,yaxis):
